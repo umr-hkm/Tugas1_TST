b2de7b0a02060a424a7dfad1e49d5c6ea95512eb
Umar Hakim <18220073@mahasiswa.itb.ac.id>
2022-09-27 14:33:33 +0700

    api dan json

--- Tugas2/rjson.py
+++ Tugas2/rjson.py
@@ -1,2 +1,20 @@
 import json
-from fastapi import fastAPI
+from fastapi import FastAPI
+
+with open("data_mhs.json", "r") as read_file:
+    data = json.load(read_file)
+
+app = FastAPI()
+
+@app.get('/')
+def homepage():
+    return "halo"
+
+@app.get('/mahasiswa/{NIM}')
+def read_mahasiswa(NIM: str):
+    for mahasiswa in data['mahasiswa']:
+        if mahasiswa['NIM'] == NIM:
+            return mahasiswa
+    raise Exception(
+        status_code=404, detail=f'Item not found'
+    )
